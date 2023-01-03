5d1ffc943c18fbd234819124a48e50fe54370820
정효윤 <rectanglehihat@gmail.com>
2023-01-03 18:28:08 +0900

    [정효윤] main index 이동

---
+++ components/main/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <iframe src="recruit_video.mp4" frameborder="0"></iframe>
+  <div class="main-wrap" :style="{ background: 'gray' }">
+    <section>
+      <h1 class="text-8xl font-bold text-white">누구든지, 어디든지</h1>
+      <h1 class="text-8xl font-bold text-white">기대 이상의</h1>
+      <h1
+        class="text-8xl font-bold bg-gradient-to-r from-secondary-green to-gradient-aqua text-transparent bg-clip-text"
+      >
+        통합 모빌리티 플랫폼
+      </h1>
+    </section>
+    <section class="mt-5">
+      <p class="text-2xl font-medium text-white">실시간으로 달라지는 교통상황까지 고려하여</p>
+      <p class="text-2xl font-medium text-white">목적지까지 최적의 이동수단으로 예약부터 결제까지</p>
+      <p class="text-2xl font-medium text-white">하나의 솔루션으로 가능한 통합 모빌리티 플랫폼 회사입니다.</p>
+    </section>
+  </div>
+</template>
