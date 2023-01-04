c1097e0a68428c86aa72123f7a3279fa588692db
yaeji <yaeji0909@gmail.com>
2023-01-04 17:26:58 +0900

    💄 upgrade navigation bar

--- components/common/navigation-bar/NavigationBar.vue
+++ components/common/navigation-bar/NavigationBar.vue
@@ -1,10 +1,10 @@
 <template>
   <nav>
     <div
-      class="nav-bar flex justify-around p-4 bg-black text-white"
+      class="nav-bar flex justify-around p-4 bg-black text-white flex items-center"
       ref="navigationBox"
     >
-      <div class="logo-box flex items-center">
+      <div class="logo-box flex">
         <NuxtLink to="/">
           <nuxt-img
             src="https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/logo-white.svg"
@@ -14,7 +14,7 @@
       <div class="menu-box">
         <ul class="flex">
           <li
-            class="mx-3 hover:text-primary-aqua cursor-pointer font-semibold"
+            class="mx-3 hover:text-primary-aqua cursor-pointer font-semibold text-xs md:text-xl xl:tex-=2xl"
             v-for="value in menu"
             :key="value"
           >
@@ -22,42 +22,65 @@
           </li>
         </ul>
       </div>
-      <div ref="langBox" class="flex justify-center items-center">
+      <div ref="buttonBox" class="flex justify-center items-center text-xs">
         <div class="hidden md:block">
           <button class="hover:text-primary-aqua">KOR&nbsp;&nbsp;|</button>
           <button class="hover:text-primary-aqua">&nbsp;&nbsp;ENG</button>
         </div>
-        <div class="md:hidden">
-          <nuxt-img
-            src="https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/lang.svg"
-          />
+        <div class="md:hidden flex items-center">
+          <button>
+            <img
+              @click="clickHandler"
+              class="relative"
+              src="https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/lang.svg"
+            />
+          </button>
+          <CommonNavigationBarHamburger />
         </div>
-        <div class="lg:hidden mx-3">
-          <nuxt-img
-            src="https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/menu.svg"
-          />
-        </div>
+      </div>
+      <div
+        v-if="langBtnIsClicked"
+        class="bg-white w-11 h-[52px] absolute top-11 right-10 z-10 rounded-sm flex flex-col justify-center text-black"
+      >
+        <button class="text-xs hover:text-primary-aqua p-1">KOR</button>
+        <button class="text-xs hover:text-primary-aqua p-1">ENG</button>
       </div>
     </div>
   </nav>
 </template>
 
 <script setup>
-import { onMounted, defineProps } from "vue";
+import { onMounted, defineProps, ref } from "vue";
 const menu = ["Service", "Company", "News", "Recruit", "Contact"];
 const props = defineProps({
   color: String,
 });
-
-const langBox = ref(null);
+const langBtnIsClicked = ref(false);
+const buttonBox = ref(null);
 const navigationBox = ref(null);
 
+const clickHandler = () => {
+  langBtnIsClicked.value = !langBtnIsClicked.value;
+};
+
 onMounted(() => {
-  console.log(navigationBox.value);
+  // console.log(navigationBox.value);
   if (props.color === "white") {
     navigationBox.value.classList.add("bg-white");
     navigationBox.value.classList.add("text-primary-aqua");
   }
-  console.log(langBox.value);
+  // console.log(langBox.value);
 });
 </script>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease-in;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
