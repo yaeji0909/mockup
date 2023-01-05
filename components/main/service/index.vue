eaad2925e980137a960ec302c9ebfe0539b3e228
정효윤 <rectanglehihat@gmail.com>
2023-01-05 11:04:51 +0900

    [정효윤] Popup btn 컴포넌트 생성

--- components/main/service/index.vue
+++ components/main/service/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>원하는 이동수단을 찜카와 함께!</div>
+  <div>
+    <p>원하는 이동수단을 찜카와 함께!</p>
+    <PopupBtn text="확인" />
+    <PopupBtn text="취소" :isCancel="true" />
+  </div>
 </template>
 
-<script setup></script>
-
-<style></style>
+<script setup>
+import PopupBtn from '/components/common/button/PopupBtn';
+</script>
