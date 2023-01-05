a78861f0588b40d1eadafdf83c482ed92bcf7bf9
정효윤 <rectanglehihat@gmail.com>
2023-01-06 08:29:12 +0900

    [정효윤] footer 컴포넌트 생성

---
+++ components/common/footer/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <footer>
+    <section>
+      <div>
+        <img src="https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/logo-black.svg" alt="" />
+        <p>회사소개</p>
+        <button>인재채용</button>
+        <button>이용약관</button>
+        <button>개인정보처리방침</button>
+      </div>
+      <div>
+        <p>주식회사 네이처 모빌리티</p>
+        <div>
+          <span>제주본사</span>
+          <span>
+            제주특별자치도 제주시 첨단로 330, 3층 카이스트 친환경 스마트 자동차 연구센터 D동 KMV10 (영평동, 세미양 빌딩)
+          </span>
+        </div>
+        <div>
+          <span>서울본사</span>
+          <span> 서울특별시 강남구 봉은사로 325, 9층 (논현동, 라임타워) </span>
+        </div>
+      </div>
+      <div>
+        <p>© Copyright 2018 - 2019 Nature Mobility | All Rights Reserved</p>
+      </div>
+    </section>
+    <section>
+      <input type="text" />
+    </section>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
