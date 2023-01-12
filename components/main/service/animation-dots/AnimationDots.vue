a0a9f9a69ddb242389cb6634e4c656548fbe7f76
rectanglehihat <80513184+rectanglehihat@users.noreply.github.com>
2023-01-13 00:45:28 +0900

    [정효윤] service AnimationDots 개발 중

---
+++ components/main/service/animation-dots/AnimationDots.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <!-- 1 -->
+    <div
+      class="flex flex-col-reverse xl:flex-row text-center xl:text-left justify-center items-center m-20 whitespace-pre-line"
+    >
+      <div class="mt-10 xl:mr-20">
+        <h1 class="text-3xl md:text-4xl xl:text-7xl font-bold">
+          {{ $t("service.1") }}
+        </h1>
+        <p class="text-xs md:text-sm xl:text-xl mt-[15px]">
+          {{ $t("service.1-desc") }}
+        </p>
+      </div>
+      <img :src="SERVICE_1" alt="SERVICE_1" class="w-[400px] md:w-[500px]" />
+    </div>
+    <!-- 2 -->
+    <div
+      class="flex flex-col xl:flex-row text-center xl:text-left justify-center items-center m-20 whitespace-pre-line"
+    >
+      <img :src="SERVICE_2" alt="SERVICE_2" class="w-[400px] md:w-[500px]" />
+      <div class="mt-10 xl:mr-20">
+        <h1 class="text-3xl md:text-4xl xl:text-7xl font-bold">
+          {{ $t("service.2") }}
+        </h1>
+        <p class="text-xs md:text-sm xl:text-xl mt-[15px]">
+          {{ $t("service.2-desc") }}
+        </p>
+      </div>
+    </div>
+    <!-- 3 -->
+    <div
+      class="flex flex-col-reverse xl:flex-row text-center xl:text-left justify-center items-center m-20 whitespace-pre-line"
+    >
+      <div class="mt-10 xl:mr-20">
+        <h1 class="text-3xl md:text-4xl xl:text-7xl font-bold">
+          {{ $t("service.3") }}
+        </h1>
+        <p class="text-xs md:text-sm xl:text-xl mt-[15px]">
+          {{ $t("service.3-desc") }}
+        </p>
+      </div>
+      <img :src="SERVICE_3" alt="SERVICE_3" class="w-[400px] md:w-[500px]" />
+    </div>
+    <!-- 4 -->
+    <div
+      class="flex flex-col xl:flex-row text-center xl:text-left justify-center items-center m-20 whitespace-pre-line"
+    >
+      <img :src="SERVICE_4" alt="SERVICE_4" class="w-[400px] md:w-[500px]" />
+      <div class="mt-10 xl:mr-20">
+        <h1 class="text-3xl md:text-4xl xl:text-7xl font-bold">
+          {{ $t("service.4") }}
+        </h1>
+        <p class="text-xs md:text-sm xl:text-xl mt-[15px]">
+          {{ $t("service.4-desc") }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const SERVICE_1 =
+  "https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/service-1.svg";
+const SERVICE_2 =
+  "https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/service-2.svg";
+const SERVICE_3 =
+  "https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/service-3.svg";
+const SERVICE_4 =
+  "https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/service-4.svg";
+</script>
--- components/main/service/index.vue
+++ components/main/service/index.vue
@@ -1,31 +1,38 @@
 <template>
-  <div class="flex flex-col items-center justify-center bg-service bg-cover py-52">
-    <h1
-      class="title opacity-0 text-3xl md:text-4xl xl:text-7xl md:leading-none font-bold text-center whitespace-pre-line md:whitespace-nowrap"
+  <div>
+    <div
+      class="flex flex-col items-center justify-center bg-service bg-cover py-52"
     >
-      {{ $t('service.title') }}
-    </h1>
-    <p
-      class="desc opacity-0 text-xs md:text-base xl:text-xl font-medium text-center whitespace-pre-line md:whitespace-nowrap"
-    >
-      {{ $t('service.desc') }}
-    </p>
-    <MainServicePhone class="phone opacity-0 mt-[40px] xl:mt-[70px]" />
+      <!-- main -->
+      <h1
+        class="title opacity-0 text-3xl md:text-4xl xl:text-7xl md:leading-none font-bold text-center whitespace-pre-line md:whitespace-nowrap"
+      >
+        {{ $t("service.title") }}
+      </h1>
+      <p
+        class="desc opacity-0 text-xs md:text-base xl:text-xl font-medium text-center whitespace-pre-line md:whitespace-nowrap"
+      >
+        {{ $t("service.desc") }}
+      </p>
+      <MainServicePhone class="phone opacity-0 mt-[40px] xl:mt-[70px]" />
+    </div>
+    <MainServiceAnimationDots />
   </div>
 </template>
 
 <script setup>
-import { gsap } from 'gsap';
-import { useI18n } from 'vue-i18n';
+import { gsap } from "gsap";
+import { useI18n } from "vue-i18n";
 
 const { t } = useI18n();
+
 /**
  * gsap animation
  */
 onMounted(() => {
   let tl = gsap.timeline(); //create the timeline
-  tl.to('.title', { opacity: 1, y: -90, ease: 'power0', duration: 0 }) //start sequencing
-    .to('.desc', { opacity: 1, y: -60, ease: 'power0', duration: 0 })
-    .to('.phone', { opacity: 1, y: -30, ease: 'power0', duration: 0 });
+  tl.to(".title", { opacity: 1, y: -90, ease: "power0", duration: 0 }) //start sequencing
+    .to(".desc", { opacity: 1, y: -60, ease: "power0", duration: 0 })
+    .to(".phone", { opacity: 1, y: -30, ease: "power0", duration: 0 });
 });
 </script>
