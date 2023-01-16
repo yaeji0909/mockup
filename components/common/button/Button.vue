c04ba07fdf0fef6271230235a2d1be4cd7733335
정효윤 <rectanglehihat@gmail.com>
2023-01-16 13:01:01 +0900

    [정효윤] common button css 수정

--- components/common/button/Button.vue
+++ components/common/button/Button.vue
@@ -3,12 +3,12 @@
     :class="[
       'text-xs md:text-base font-bold rounded-full',
       icon
-        ? 'flex justify-center items-center gap-2.5 w-[180px] h-[45px] md:h-[50px] hover:bg-white hover:text-black'
-        : 'w-[190px] h-[45px] md:h-[58px] hover:bg-gradient-to-r from-primary-aqua to-primary-mint hover:border-none',
+        ? 'flex justify-center items-center gap-2.5 w-[180px] h-[45px] md:h-[50px] bg-white/30 hover:bg-white hover:text-black'
+        : 'w-[190px] h-[45px] md:h-[58px] hover:bg-gradient-to-r from-primary-aqua to-primary-mint text-white hover:border-none hover:text-white',
       ,
-      bgColor ? `bg-${bgColor}` : 'bg-white/30',
-      bdColor ? `border-${bdColor}` : 'border',
-      textColor ? `text-${textColor}` : 'text-white',
+      `bg-${bgColor}` || 'bg-white/30',
+      `border border-${bdColor}` || 'border',
+      `text-${textColor}` || 'text-white',
     ]"
     @click="$emit('click', $event)"
   >
