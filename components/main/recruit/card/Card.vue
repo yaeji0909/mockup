d560fba31a0998505a2f3a58fab9e5b2fd4ee325
정효윤 <rectanglehihat@gmail.com>
2023-01-17 14:26:52 +0900

    [정효윤] recruit card swiper 적용

--- components/main/recruit/card/Card.vue
+++ components/main/recruit/card/Card.vue
@@ -1,42 +1,49 @@
 <template>
-  <div class="bg-secondary-beige h-screen px-10 pt-20">
+  <div class="bg-secondary-beige h-screen">
     <!-- status -->
-    <!-- <section class="flex justify-center items-center"> -->
-    <swiper :grabCursor="true" :spaceBetween="2" :modules="modules">
+    <swiper :grabCursor="true" :slidesPerView="5" :modules="modules" class="board w-[900px]">
       <swiper-slide
         v-for="(board, idx) in recruitBoardList"
         :key="board.department"
-        class="flex justify-center items-center gap-2 w-[50px]"
+        class="board flex justify-center items-center gap-2"
       >
         <p class="text-base font-medium">{{ board.department }}</p>
         <p
           class="text-xs font-bold rounded-full w-[29px] h-[24px] flex justify-center items-center"
-          :class="
-            idx === 0
-              ? ['bg-black', 'text-white']
-              : ['bg-gray-border', 'text-black']
-          "
+          :class="idx === 0 ? ['bg-black', 'text-white'] : ['bg-gray-border', 'text-black']"
         >
           {{ board.num }}
         </p>
       </swiper-slide>
     </swiper>
-    <!-- </section> -->
     <!-- card -->
     <swiper
       :grabCursor="true"
-      :slidesPerView="3"
+      :slidesPerView="auto"
+      :centeredSlides="largerThanSm ? false : true"
       :spaceBetween="30"
       :modules="modules"
       :pagination="{
         clickable: true,
       }"
+      :breakpoints="{
+        320: {
+          slidesPerView: 'auto',
+        },
+        768: {
+          slidesPerView: 'auto',
+        },
+        1024: {
+          slidesPerView: 'auto',
+        },
+      }"
+      class="card"
     >
       <!-- <div class="flex justify-center gap-10 pt-12"> -->
       <swiper-slide
         v-for="r in recruit"
         :key="r.title"
-        class="bg-white w-[260px] h-[180px] md:w-[304px] md:h-[210px] lg:w-[372px] lg:h-[260px] rounded-[20px] shadow-recruit px-[35px] py-[30px] my-12"
+        class="card bg-white rounded-[20px] shadow-recruit mt-10 mb-10 md:my-12"
       >
         <div class="flex justify-between items-center">
           <p class="text-xs md:text-base lg:text-xl font-medium text-gray-sub">
@@ -48,17 +55,13 @@
             {{ r.career }}
           </p>
         </div>
-        <h3
-          class="recruit-title text-xl md:text-2xl xl:text-3xl font-bold text-black pt-[13px] overflow-hidden"
-        >
+        <h3 class="recruit-title text-xl md:text-2xl xl:text-3xl font-bold text-black pt-[13px] overflow-hidden">
           {{ r.title }}
         </h3>
         <p class="text-xs md:text-sm xl:text-base text-gray-caption pt-[10px]">
           {{ r.location }}
         </p>
-        <h3
-          class="text-xl md:text-2xl xl:text-3xl font-bold text-primary-aqua pt-[23px]"
-        >
+        <h3 class="text-xl md:text-2xl xl:text-3xl font-bold text-primary-aqua pt-[12px] xl:pt-[23px]">
           {{ r.dueDate }}
         </h3>
       </swiper-slide>
@@ -71,15 +74,17 @@
 import recruits from '@/components/main/recruit/card/recruits.json';
 import { useI18n } from 'vue-i18n';
 import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Pagination, Mousewheel } from 'swiper';
+import { Pagination, Mousewheel, FreeMode } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/free-mode';
+import { breakpointsTailwind, useBreakpoints } from '@vueuse/core';
 
 const { t } = useI18n();
 
 const { recruit } = recruits;
 
-const modules = [Pagination, Mousewheel];
+const modules = [Pagination, Mousewheel, FreeMode];
 
 const recruitBoardList = [
   { department: t('recruit.total'), num: 5 },
@@ -88,19 +93,59 @@ const recruitBoardList = [
   { department: t('recruit.engineer'), num: 2 },
   { department: t('recruit.cs'), num: 1 },
 ];
+
+const breakpoints = useBreakpoints(breakpointsTailwind);
+const largerThanSm = breakpoints.greater('sm'); // only larger than sm
 </script>
 
 <style scoped>
 .recruit-title {
-  /* transform: translate(0px, 0px); */
   display: -webkit-box;
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
 }
-/* custom pagination */
+:deep(.swiper) {
+  padding: 0px 30px;
+}
 :deep(.swiper-pagination) {
   bottom: 0%;
+  width: 100%;
 }
+/* fix width */
+@media (min-width: 1025px) {
+  :deep(.swiper-slide.board) {
+    height: 50px;
+  }
+  :deep(.swiper-slide.card) {
+    width: 372px !important;
+    /* width: 30% !important; */
+    height: 260px !important;
+    padding: 30px 35px;
+  }
+}
+@media (min-width: 768px) and (max-width: 1024px) {
+  :deep(.swiper-slide.board) {
+    height: 50px;
+  }
+  :deep(.swiper-slide.card) {
+    width: 304px !important;
+    /* width: 30% !important; */
+    height: 210px !important;
+    padding: 20px 25px;
+  }
+}
+@media (max-width: 767px) {
+  :deep(.swiper-slide.board) {
+    height: 50px;
+  }
+  :deep(.swiper-slide.card) {
+    width: 260px !important;
+    /* width: 30% !important; */
+    height: 180px !important;
+    padding: 15px 20px;
+  }
+}
+/* custom pagination */
 :deep(.swiper-pagination-bullet) {
   width: 8px;
   height: 8px;
