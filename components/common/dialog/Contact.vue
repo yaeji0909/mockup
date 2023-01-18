b1f9bcaff6c9f1cc9cb3f608086b6051b38c5a0a
rectanglehihat <80513184+rectanglehihat@users.noreply.github.com>
2023-01-19 00:30:56 +0900

    [정효윤] dialog components 개발 중

--- components/common/dialog/Contact.vue
+++ components/common/dialog/Contact.vue
@@ -1,11 +1,69 @@
 <template>
-  <form class="h-[750px] bg-white px-5 py-6 rounded-[20px]">
-    <h1>문의하기</h1>
-    <div>
-      <label for="company">회사명</label>
-      <CommonInput placeholder="회사명을 입력해주세요." />
-    </div>
+  <form
+    :class="[
+      'h-[750px] bg-white rounded-[20px]',
+      !largerThanSm ? 'h-screen w-screen rounded-none' : '',
+    ]"
+  >
+    <section>
+      <div class="flex justify-between items-center px-5 py-6">
+        <img v-if="!largerThanSm" :src="ARROW" alt="CLOSE" class="w-[20px]" />
+        <h1
+          :class="[
+            'text-xl md:text-4xl xl:text-5xl font-bold',
+            !largerThanSm ? 'm-auto ' : '',
+          ]"
+        >
+          {{ $t('dialog.title') }}
+        </h1>
+        <img v-if="largerThanSm" :src="CLOSE" alt="CLOSE" class="w-[19px]" />
+      </div>
+      <hr v-if="!largerThanSm" class="border-gray-border" />
+    </section>
+    <section class="px-5 py-6">
+      <div v-for="input in inputList" :key="input.label" class="mb-5">
+        <label for="company" class="text-base xl:text-xl font-medium">
+          {{ input.label }}
+        </label>
+        <CommonInput
+          :placeholder="input.placeholder"
+          class="mt-[10px] w-full"
+        />
+      </div>
+      <CommonCheckBox :text="$t('dialog.agreeText')" />
+    </section>
+    <section class="px-5 pb-6 flex justify-center">
+      <CommonButton
+        :text="$t('dialog.submit')"
+        bgColor="primary-aqua"
+        :icon="false"
+        class="w-1/2"
+      />
+    </section>
   </form>
 </template>
 
-<script setup></script>
+<script setup>
+import { useI18n } from 'vue-i18n';
+import { breakpointsTailwind, useBreakpoints } from '@vueuse/core';
+
+const CLOSE =
+  'https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/dialog_close.svg';
+const ARROW =
+  'https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/dialog_arrow.svg';
+
+const { t } = useI18n();
+
+const inputList = ref([
+  { label: t('dialog.company'), placeholder: t('dialog.companyPlaceholder') },
+  { label: t('dialog.name'), placeholder: t('dialog.namePlaceholder') },
+  { label: t('dialog.mobile'), placeholder: t('dialog.mobilePlaceholder') },
+  { label: t('dialog.email'), placeholder: t('dialog.emailPlaceholder') },
+  { label: t('dialog.type'), placeholder: t('dialog.typePlaceholder') },
+  { label: t('dialog.content'), placeholder: t('dialog.contentPlaceholder') },
+  { label: t('dialog.privacy'), placeholder: t('dialog.privacyText') },
+]);
+
+const breakpoints = useBreakpoints(breakpointsTailwind);
+const largerThanSm = breakpoints.greater('sm'); // only larger than sm
+</script>
