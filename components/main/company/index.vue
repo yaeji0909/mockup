3f12c23615182dda0dffc261cc81d857e8b2b0e6
rectanglehihat <80513184+rectanglehihat@users.noreply.github.com>
2023-01-19 23:43:41 +0900

    [정효윤] company slide 개발 중

--- components/main/company/index.vue
+++ components/main/company/index.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="relative w-full h-screen">
+  <div class="relative w-full h-screen text-white">
     <section>
-      <video autoplay loop muted class="absolute z-[-1] w-full h-full object-cover brightness-50">
+      <video
+        autoplay
+        loop
+        muted
+        class="absolute z-[-1] w-full h-full object-cover brightness-50"
+      >
         <source :src="COMPANY_VIDEO" type="video/mp4" />
       </video>
     </section>
     <div class="flex justify-center">
       <!-- slide 1 (2022) -->
-      <section class="text-white pt-[100px] w-[375px] md:w-[704px] xl:w-[1200px]">
+      <!-- <section class="pt-[100px] w-[375px] md:w-[704px] xl:w-[1200px]">
         <div>
           <h1 class="text-4xl md:text-6xl xl:text-7xl font-bold">Company</h1>
-          <h5 class="text-base xl:text-xl mt-[5px] md:mt-[10px]">{{ $t('company.sub') }}</h5>
+          <h5 class="text-base xl:text-xl mt-[5px] md:mt-[10px]">
+            {{ $t('company.sub') }}
+          </h5>
         </div>
-        <div class="flex flex-col xl:flex-row justify-between mt-10 xl:mt-20 gap-4 md:gap-6">
+        <div
+          class="flex flex-col xl:flex-row justify-between mt-10 xl:mt-20 gap-4 md:gap-6"
+        >
           <h1
             class="text-4xl md:text-6xl xl:text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary-aqua to-primary-mint"
           >
@@ -21,34 +30,34 @@
           <div>
             <div v-for="year in historyList['2022']" :key="year.month">
               <div
-                :class="['flex items-center gap-5 font-medium border-b w-fit', year.month === '05' ? 'py-5' : 'py-3']"
+                :class="[
+                  'flex items-center gap-5 font-medium border-b w-fit',
+                  year.month === '05' ? 'py-5' : 'py-3',
+                ]"
               >
-                <h3 class="text-base md:text-xl xl:text-3xl">{{ year.month }}</h3>
-                <div v-if="year.month === '05'" class="flex text-base md:text-2xl xl:text-5xl">
-                  <!-- 중소벤처기업부 -->
-                  <div v-if="year.idx === 1">
-                    <!-- <h3>{{ year.content }}</h3> -->
-                    <h3>{{ $t('company.2022.05-1') }}</h3>
-                  </div>
-                  <!-- 아기 유니콘 -->
-                  <div v-if="year.idx === 2" class="relative">
+                <h3 class="text-base md:text-xl xl:text-3xl">
+                  {{ year.month }}
+                </h3>
+                <div
+                  v-if="year.month === '05'"
+                  class="flex items-center text-base md:text-2xl xl:text-5xl"
+                >
+                  <h3 class="mr-2">{{ $t('company.2022.05-1') }}</h3>
+                  <div class="relative">
                     <img
                       :src="STAR"
                       alt="STAR"
-                      class="absolute top-[-25%] left-[50%] transform -translate-x-1/2 -translate-y-1/2"
+                      class="absolute top-[-10%] left-[50%] transform -translate-x-1/2 -translate-y-1/2"
                     />
-                    <div class="flex">
+                    <div class="flex items-center">
                       <img :src="FEATHER" alt="FEATHER" />
-                      <!-- <h3 class="text-primary-mint drop-shadow-history">{{ year.content }}</h3> -->
-                      <h3 class="text-primary-mint drop-shadow-history">{{ $t('company.2022.05-2') }}</h3>
+                      <h3 class="text-primary-mint drop-shadow-history">
+                        {{ $t('company.2022.05-2') }}
+                      </h3>
                       <img :src="FEATHER" alt="FEATHER" class="-scale-x-100" />
                     </div>
                   </div>
-                  <!-- 기업 1위 선정 -->
-                  <div v-if="year.idx === 3">
-                    <!-- <h3>{{ year.content }}</h3> -->
-                    <h3>{{ $t('company.2022.05-3') }}</h3>
-                  </div>
+                  <h3 class="ml-2">{{ $t('company.2022.05-3') }}</h3>
                 </div>
                 <h3 v-else class="text-xs md:text-base xl:text-2xl">
                   {{ year.content }}
@@ -57,6 +66,102 @@
             </div>
           </div>
         </div>
+      </section> -->
+      <!-- slide 2 (2021-2020) -->
+      <!-- <section class="pt-[100px] w-[375px] md:w-[704px] xl:w-[1200px]">
+        <article
+          class="flex flex-col xl:flex-row justify-between mt-10 xl:mt-20 gap-4 md:gap-6"
+        >
+          <h1
+            class="text-4xl md:text-6xl xl:text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary-aqua to-primary-mint"
+          >
+            2021
+          </h1>
+          <div class="xl:w-[600px]">
+            <div v-for="year in historyList['2021']" :key="year.month">
+              <div class="flex items-baseline gap-5 font-medium border-b w-fit">
+                <h3 class="text-base md:text-xl xl:text-3xl">
+                  {{ year.month }}
+                </h3>
+                <h3
+                  class="text-xs md:text-base xl:text-2xl whitespace-pre-line py-3"
+                >
+                  {{ year.content }}
+                </h3>
+              </div>
+            </div>
+          </div>
+        </article>
+        <article
+          class="flex flex-col xl:flex-row justify-between mt-10 xl:mt-10 gap-4 md:gap-6"
+        >
+          <h1
+            class="text-4xl md:text-6xl xl:text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary-aqua to-primary-mint"
+          >
+            2020
+          </h1>
+          <div>
+            <div v-for="year in historyList['2020']" :key="year.month">
+              <div class="flex items-center gap-5 font-medium border-b w-fit">
+                <h3 class="text-base md:text-xl xl:text-3xl">
+                  {{ year.month }}
+                </h3>
+                <h3 class="text-xs md:text-base xl:text-2xl py-3">
+                  {{ year.content }}
+                </h3>
+              </div>
+            </div>
+          </div>
+        </article>
+      </section> -->
+      <!-- slide 3 (2019-2018) -->
+      <section class="pt-[100px] w-[375px] md:w-[704px] xl:w-[1200px]">
+        <article
+          class="flex flex-col xl:flex-row justify-between mt-10 xl:mt-20 gap-4 md:gap-6"
+        >
+          <h1
+            class="text-4xl md:text-6xl xl:text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary-aqua to-primary-mint"
+          >
+            2019
+          </h1>
+          <div class="xl:w-[600px]">
+            <div v-for="year in historyList['2019']" :key="year.month">
+              <div class="flex items-baseline gap-5 font-medium border-b w-fit">
+                <h3 class="text-base md:text-xl xl:text-3xl">
+                  {{ year.month }}
+                </h3>
+                <h3
+                  class="text-xs md:text-base xl:text-2xl whitespace-pre-line py-3"
+                >
+                  {{ year.content }}
+                </h3>
+              </div>
+            </div>
+          </div>
+        </article>
+        <article
+          class="flex flex-col xl:flex-row justify-between mt-10 xl:mt-10 gap-4 md:gap-6"
+        >
+          <h1
+            class="text-4xl md:text-6xl xl:text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary-aqua to-primary-mint"
+          >
+            2020
+          </h1>
+          <div class="xl:w-[600px]">
+            <div v-for="year in historyList['2018']" :key="year.month">
+              <div class="flex items-baseline gap-5 font-medium border-b w-fit">
+                <h3 class="text-base md:text-xl xl:text-3xl">
+                  {{ year.month }}
+                </h3>
+                <h3
+                  class="text-xs md:text-base xl:text-2xl whitespace-pre-line py-3"
+                >
+                  {{ year.content }}
+                </h3>
+              </div>
+            </div>
+          </div>
+        </article>
       </section>
     </div>
   </div>
@@ -65,19 +170,26 @@
 <script setup>
 import { useI18n } from 'vue-i18n';
 
-const COMPANY_VIDEO = 'https://naturemobility.s3.ap-northeast-2.amazonaws.com/video/history_video.mp4';
-const STAR = 'https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/history_star.svg';
-const FEATHER = 'https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/history_feather.svg';
+const COMPANY_VIDEO =
+  'https://naturemobility.s3.ap-northeast-2.amazonaws.com/video/history_video.mp4';
+const STAR =
+  'https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/history_star.svg';
+const FEATHER =
+  'https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/history_feather.svg';
 
 const { t } = useI18n();
 const historyList = ref({
   2022: [
     { month: '10', content: t('company.2022.10') },
     { month: '07', content: t('company.2022.07') },
-    { month: '05', content: t('company.2022.05') },
-    // { month: '05', content: t('company.2022.05-1'), idx: 1 },
-    // { month: '05', content: t('company.2022.05-2'), idx: 2 },
-    // { month: '05', content: t('company.2022.05-3'), idx: 3 },
+    {
+      month: '05',
+      // content: [
+      //   t('company.2022.05-1'),
+      //   t('company.2022.05-2'),
+      //   t('company.2022.05-3'),
+      // ],
+    },
     { month: '04', content: t('company.2022.04') },
     { month: '02', content: t('company.2022.02') },
     { month: '01', content: t('company.2022.01') },
