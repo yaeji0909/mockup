b74150103f857e6c05613936d14b43871200a11c
rectanglehihat <80513184+rectanglehihat@users.noreply.github.com>
2023-01-29 22:55:36 +0900

    [정효윤] Footer에 Contact dialog 추가

--- components/common/dialog/Contact.vue
+++ components/common/dialog/Contact.vue
@@ -1,12 +1,35 @@
 <template>
-  <form :class="['h-[750px] bg-white rounded-[20px] ', !largerThanSm ? 'h-screen w-screen rounded-none' : '']">
+  <form
+    :class="[
+      !largerThanSm
+        ? 'h-screen w-screen rounded-none'
+        : 'h-[750px] bg-white rounded-[20px] overflow-auto',
+    ]"
+  >
     <section>
       <div class="flex justify-between items-center px-5 py-6">
-        <img v-if="!largerThanSm" :src="ARROW" alt="CLOSE" class="w-[20px]" @click="$emit('close')" />
-        <h1 :class="['text-xl md:text-4xl xl:text-5xl font-bold', !largerThanSm ? 'm-auto ' : '']">
+        <img
+          v-if="!largerThanSm"
+          :src="ARROW"
+          alt="CLOSE"
+          class="w-[20px]"
+          @click="$emit('close')"
+        />
+        <h1
+          :class="[
+            'text-xl md:text-4xl xl:text-5xl font-bold',
+            !largerThanSm ? 'm-auto ' : '',
+          ]"
+        >
           {{ $t('dialog.title') }}
         </h1>
-        <img v-if="largerThanSm" :src="CLOSE" alt="CLOSE" class="w-[19px] cursor-pointer" @click="$emit('close')" />
+        <img
+          v-if="largerThanSm"
+          :src="CLOSE"
+          alt="CLOSE"
+          class="w-[19px] cursor-pointer"
+          @click="$emit('close')"
+        />
       </div>
       <hr v-if="!largerThanSm" class="border-gray-border" />
     </section>
@@ -15,12 +38,23 @@
         <label for="company" class="text-base xl:text-xl font-medium">
           {{ input.label }}
         </label>
-        <CommonInput :placeholder="input.placeholder" class="mt-[10px] w-full" v-model:input.content="value" />
+        <SelectBox v-if="input.label === '문의 유형'" />
+        <CommonInput
+          v-else
+          :placeholder="input.placeholder"
+          class="mt-[10px] w-full"
+          v-model:input.content="value"
+        />
       </div>
       <CheckBox :text="$t('dialog.agreeText')" />
     </section>
     <section class="px-5 pb-6 flex justify-center">
-      <CommonButton :text="$t('dialog.submit')" bgColor="primary-aqua" :icon="false" class="w-1/2" />
+      <CommonButton
+        :text="$t('dialog.submit')"
+        bgColor="primary-aqua"
+        :icon="false"
+        class="w-1/2"
+      />
     </section>
     <!-- <CommonDialogAlert /> -->
     <!-- <CommonDialogSuccess /> -->
@@ -29,12 +63,14 @@
 
 <script setup>
 import CheckBox from '/components/common/checkbox/CheckBox';
+import SelectBox from '/components/common/selectbox/SelectBox';
 import { useI18n } from 'vue-i18n';
 import { breakpointsTailwind, useBreakpoints } from '@vueuse/core';
-import { onMounted, ref, reactive } from 'vue';
 
-const CLOSE = 'https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/dialog_close.svg';
-const ARROW = 'https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/dialog_arrow.svg';
+const CLOSE =
+  'https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/dialog_close.svg';
+const ARROW =
+  'https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/dialog_arrow.svg';
 
 const company = ref('');
 
@@ -44,13 +80,41 @@ const company = ref('');
 const { t } = useI18n();
 
 const inputList = ref([
-  { label: t('dialog.company'), placeholder: t('dialog.companyPlaceholder'), content: '' },
-  { label: t('dialog.name'), placeholder: t('dialog.namePlaceholder'), content: '' },
-  { label: t('dialog.mobile'), placeholder: t('dialog.mobilePlaceholder'), content: '' },
-  { label: t('dialog.email'), placeholder: t('dialog.emailPlaceholder'), content: '' },
-  { label: t('dialog.type'), placeholder: t('dialog.typePlaceholder'), content: '' },
-  { label: t('dialog.content'), placeholder: t('dialog.contentPlaceholder'), content: '' },
-  { label: t('dialog.privacy'), placeholder: t('dialog.privacyText'), content: '' },
+  {
+    label: t('dialog.company'),
+    placeholder: t('dialog.companyPlaceholder'),
+    content: '',
+  },
+  {
+    label: t('dialog.name'),
+    placeholder: t('dialog.namePlaceholder'),
+    content: '',
+  },
+  {
+    label: t('dialog.mobile'),
+    placeholder: t('dialog.mobilePlaceholder'),
+    content: '',
+  },
+  {
+    label: t('dialog.email'),
+    placeholder: t('dialog.emailPlaceholder'),
+    content: '',
+  },
+  {
+    label: t('dialog.type'),
+    placeholder: t('dialog.typePlaceholder'),
+    content: '',
+  },
+  {
+    label: t('dialog.content'),
+    placeholder: t('dialog.contentPlaceholder'),
+    content: '',
+  },
+  {
+    label: t('dialog.privacy'),
+    placeholder: t('dialog.privacyText'),
+    content: '',
+  },
 ]);
 
 /**
