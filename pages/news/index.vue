8be8260ef30a7358ae6ac0f3451f671ef3acc0ed
rectanglehihat <80513184+rectanglehihat@users.noreply.github.com>
2023-01-29 22:26:23 +0900

    [정효윤] news 페이지 NavigationBar 추가

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <CommonNavigationBar class="fixed z-10 w-screen" :elementTop="elementTop" />
+    <CommonNavigationBar
+      class="fixed z-10 w-screen"
+      :elementTop="elementTop"
+      color="white"
+    />
     <main class="w-[335px] md:w-[710px] xl:w-[1200px] m-auto">
       <News />
     </main>
