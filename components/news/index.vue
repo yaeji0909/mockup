308b57e9d1094abe73f370551e012aa4913c3051
정효윤 <rectanglehihat@gmail.com>
2023-01-30 09:49:41 +0900

    [정효윤] SearchInput 폴더 구조 변경

--- components/news/index.vue
+++ components/news/index.vue
@@ -2,30 +2,24 @@
   <div>
     <main class="w-[335px] md:w-[710px] xl:w-[1200px] m-auto">
       <div class="pt-40">
-        <NewsShearchInput :total="total" />
+        <CommonInputSearch :total="total" />
       </div>
       <div class="mt-[40px] xl:mt-[50px] border border-gray-sub" />
       <ClientOnly>
         <MainNewsCard :news="news" :isNewsPage="true" />
-        <CommonPagination
-          class="mt-40"
-          :news="news"
-          :currentPage="currentPage"
-          @change="changePage"
-        />
+        <CommonPagination class="mt-40" :news="news" :currentPage="currentPage" @change="changePage" />
       </ClientOnly>
     </main>
   </div>
 </template>
 
 <script setup>
-import NewsShearchInput from '/components/news/search-input/SearchInput';
 import { total, newsList } from '/components/news/news.json';
 import { newsList2 } from '/components/news/news2.json';
 
 let news = ref(newsList);
 let currentPage = ref(1);
-const changePage = (param) => {
+const changePage = param => {
   if (param === 1) news.value = newsList;
   if (param === 2) news.value = newsList2;
   currentPage.value = param;
