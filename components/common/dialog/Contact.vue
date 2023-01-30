b65b1d9571e879f4eec30c7c98b83ab6c509b8b1
정효윤 <rectanglehihat@gmail.com>
2023-01-30 13:07:03 +0900

    [정효윤] checkBox selectBox common으로 자동 import

--- components/common/dialog/Contact.vue
+++ components/common/dialog/Contact.vue
@@ -1,35 +1,12 @@
 <template>
-  <div
-    :class="[
-      !largerThanSm
-        ? 'h-screen w-screen rounded-none'
-        : 'h-[750px] bg-white rounded-[20px] overflow-auto',
-    ]"
-  >
+  <div :class="[!largerThanSm ? 'h-screen w-screen rounded-none' : 'h-[750px] bg-white rounded-[20px] overflow-auto']">
     <section>
       <div class="flex justify-between items-center px-5 py-6">
-        <img
-          v-if="!largerThanSm"
-          :src="ARROW"
-          alt="CLOSE"
-          class="w-[20px]"
-          @click="$emit('close')"
-        />
-        <h1
-          :class="[
-            'text-xl md:text-4xl xl:text-5xl font-bold',
-            !largerThanSm ? 'm-auto ' : '',
-          ]"
-        >
+        <img v-if="!largerThanSm" :src="ARROW" alt="CLOSE" class="w-[20px]" @click="$emit('close')" />
+        <h1 :class="['text-xl md:text-4xl xl:text-5xl font-bold', !largerThanSm ? 'm-auto ' : '']">
           {{ $t('dialog.title') }}
         </h1>
-        <img
-          v-if="largerThanSm"
-          :src="CLOSE"
-          alt="CLOSE"
-          class="w-[19px] cursor-pointer"
-          @click="$emit('close')"
-        />
+        <img v-if="largerThanSm" :src="CLOSE" alt="CLOSE" class="w-[19px] cursor-pointer" @click="$emit('close')" />
       </div>
       <hr v-if="!largerThanSm" class="border-gray-border" />
     </section>
@@ -38,24 +15,19 @@
         <label for="company" class="text-base xl:text-xl font-medium">
           {{ input.label }}
         </label>
-        <SelectBox v-if="input.label === '문의 유형'" />
+        <CommonSelectBox v-if="input.label === '문의 유형'" />
         <CommonInput
           v-else
           :placeholder="input.placeholder"
           class="mt-[10px] w-full"
           :value="input.content"
-          @input="(e) => (input.content = e.target.value)"
+          @input="e => (input.content = e.target.value)"
         />
       </div>
-      <CheckBox :text="$t('dialog.agreeText')" />
+      <CommonCheckBox :text="$t('dialog.agreeText')" />
     </section>
     <section class="px-5 pb-6 flex justify-center">
-      <CommonButton
-        :text="$t('dialog.submit')"
-        bgColor="primary-aqua"
-        :icon="false"
-        class="w-1/2"
-      />
+      <CommonButton :text="$t('dialog.submit')" bgColor="primary-aqua" :icon="false" class="w-1/2" />
     </section>
     <!-- <CommonDialogAlert /> -->
     <!-- <CommonDialogSuccess /> -->
@@ -63,16 +35,12 @@
 </template>
 
 <script setup>
-import CheckBox from '/components/common/checkbox/CheckBox';
-import SelectBox from '/components/common/selectbox/SelectBox';
 import { useI18n } from 'vue-i18n';
 import { breakpointsTailwind, useBreakpoints } from '@vueuse/core';
 import { ref, watch, onMounted } from 'vue';
 
-const CLOSE =
-  'https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/dialog_close.svg';
-const ARROW =
-  'https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/dialog_arrow.svg';
+const CLOSE = 'https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/dialog_close.svg';
+const ARROW = 'https://naturemobility.s3.ap-northeast-2.amazonaws.com/image/dialog_arrow.svg';
 
 /**
  * setting forma
