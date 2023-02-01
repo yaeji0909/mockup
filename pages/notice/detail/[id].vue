cb7210a992290170c51608d4d8a7e96d92a9bbfd
rectanglehihat <80513184+rectanglehihat@users.noreply.github.com>
2023-02-01 23:53:19 +0900

    [정효윤] notice deail page 개발

--- pages/notice/detail/[id].vue
+++ pages/notice/detail/[id].vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="w-[335px] md:w-[710px] xl:w-[1200px] m-auto">
+  <div
+    class="w-[335px] md:w-[710px] xl:w-[1200px] m-auto"
+    v-for="info in infoList"
+    :key="info.pid"
+  >
     <section>
-      <div class="flex justify-center items-center text-base xl:text-xl font-medium">
-        <h3 class="p-[10px]">공지 사항</h3>
+      <div
+        class="flex justify-center items-center text-base xl:text-xl font-medium"
+      >
+        <h3 class="p-[10px]">{{ $t('notice.notice') }}</h3>
         <div class="border-r w-[2px] h-[10px]" />
-        <h3 class="p-[10px]">2022.09.01</h3>
+        <h3 class="p-[10px]">{{ info.date }}</h3>
       </div>
       <div class="text-center">
-        <h1 class="text-3xl md:text-4xl xl:text-7xl font-bold">신주발행 공고</h1>
+        <h1 class="text-3xl md:text-4xl xl:text-7xl font-bold">
+          {{ info.title }}
+        </h1>
       </div>
     </section>
     <hr class="bg-gray-sub h-[2px] my-[40px] md:my-[60px]" />
     <section>
-      <div>contents...</div>
+      <div v-html="info.contents" />
     </section>
   </div>
 </template>
@@ -21,12 +29,15 @@
 import { useNoticeStore } from '/stores/notice';
 import { storeToRefs } from 'pinia';
 
-const route = useRoute();
-console.log('route.param', route.param);
+/**
+ * get notice info with the same id as route.params.id from pinia store
+ */
 const noticeStore = useNoticeStore();
 const { noticeList } = storeToRefs(noticeStore);
 
-noticeList.value.map(n => {
-  console.log('n.pid', n.pid);
-});
+const route = useRoute();
+
+const infoList = noticeList.value.filter(
+  (n) => n.pid === Number(route.params.id)
+);
 </script>
