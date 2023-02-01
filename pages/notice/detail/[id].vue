1a85b8d75212585425de032fada0b8c7e72dae0e
rectanglehihat <80513184+rectanglehihat@users.noreply.github.com>
2023-02-02 00:34:57 +0900

    [정효윤] notice detail page 개발

--- pages/notice/detail/[id].vue
+++ pages/notice/detail/[id].vue
@@ -1,27 +1,42 @@
 <template>
-  <div
-    class="w-[335px] md:w-[710px] xl:w-[1200px] m-auto"
-    v-for="info in infoList"
-    :key="info.pid"
-  >
-    <section>
-      <div
-        class="flex justify-center items-center text-base xl:text-xl font-medium"
-      >
-        <h3 class="p-[10px]">{{ $t('notice.notice') }}</h3>
-        <div class="border-r w-[2px] h-[10px]" />
-        <h3 class="p-[10px]">{{ info.date }}</h3>
-      </div>
-      <div class="text-center">
-        <h1 class="text-3xl md:text-4xl xl:text-7xl font-bold">
-          {{ info.title }}
-        </h1>
-      </div>
-    </section>
-    <hr class="bg-gray-sub h-[2px] my-[40px] md:my-[60px]" />
-    <section>
-      <div v-html="info.contents" />
-    </section>
+  <div>
+    <div class="w-[335px] md:w-[710px] xl:w-[1200px] m-auto">
+      <section v-for="info in infoList" :key="info.pid">
+        <div>
+          <div
+            class="flex justify-center items-center text-base xl:text-xl font-medium"
+          >
+            <h3 class="p-[10px]">{{ $t('notice.notice') }}</h3>
+            <div class="border-r w-[2px] h-[12px]" />
+            <h3 class="p-[10px]">{{ info.date }}</h3>
+          </div>
+          <div class="text-center">
+            <h1 class="text-3xl md:text-4xl xl:text-7xl font-bold">
+              {{ info.title }}
+            </h1>
+          </div>
+        </div>
+        <hr class="bg-gray-sub h-[2px] my-[40px] md:my-[60px]" />
+        <div>
+          <div v-html="info.contents" />
+        </div>
+      </section>
+      <section>
+        <CommonPrevNext />
+      </section>
+      <section class="text-center">
+        <CommonButton
+          bgColor="black"
+          :text="$t('notice.toList')"
+          :icon="false"
+          class="mt-20"
+          @click="moveToNoticeList"
+        />
+      </section>
+    </div>
+    <div class="mt-[100px]">
+      <CommonFooter />
+    </div>
   </div>
 </template>
 
@@ -40,4 +55,13 @@ const route = useRoute();
 const infoList = noticeList.value.filter(
   (n) => n.pid === Number(route.params.id)
 );
+
+/**
+ * move to notice list
+ */
+const router = useRouter();
+
+const moveToNoticeList = () => {
+  router.push('/notice');
+};
 </script>
