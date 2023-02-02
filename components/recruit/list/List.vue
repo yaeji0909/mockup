565f1308dd521ccdf017135d965b3a6852439369
rectanglehihat <80513184+rectanglehihat@users.noreply.github.com>
2023-02-02 23:34:28 +0900

    [정효윤] 검색 색상 변경 구현 중

--- components/recruit/list/List.vue
+++ components/recruit/list/List.vue
@@ -9,16 +9,27 @@
         @click="moveToRecruit(recruit.recruitUrl)"
       >
         <div class="flex-row items-center lg:flex pb-[20px] md:pb-0">
-          <h3 class="text-base md:text-xl xl:text-2xl font-medium w-40 text-left pb-[20px] md:pb-0">
+          <h3
+            class="text-base md:text-xl xl:text-2xl font-medium w-40 text-left pb-[20px] md:pb-0"
+          >
             {{ recruit.department }}
           </h3>
           <div class="text-left md:w-[40rem] lg:w-[30rem] xl:w-[60rem]">
-            <h1 ref="recruitTitle" class="recruit-title text-2xl xl:text-3xl font-bold pb-[10px] truncate">
+            <h1
+              ref="recruitTitle"
+              class="recruit-title text-2xl xl:text-3xl font-bold pb-[10px] truncate"
+            >
               {{ recruit.title }}
             </h1>
             <div class="flex items-center gap-[15px]">
-              <h5 class="text-base md:text-xl xl:text-2xl font-medium text-primary-aqua">D-{{ recruit.dDay }}</h5>
-              <h5 class="text-xs md:text-sm xl:text-base text-gray-sub">{{ recruit.startAt }} ~ {{ recruit.endAt }}</h5>
+              <h5
+                class="text-base md:text-xl xl:text-2xl font-medium text-primary-aqua"
+              >
+                D-{{ recruit.dDay }}
+              </h5>
+              <h5 class="text-xs md:text-sm xl:text-base text-gray-sub">
+                {{ recruit.startAt }} ~ {{ recruit.endAt }}
+              </h5>
             </div>
           </div>
         </div>
@@ -49,7 +60,7 @@ const largerThanSm = breakpoints.greater('sm'); // only larger than sm
 /**
  * 해당 공고로 이동
  */
-const moveToRecruit = url => {
+const moveToRecruit = (url) => {
   console.log('리크루트로 이동');
   if (!url) return;
 };
@@ -57,13 +68,13 @@ const moveToRecruit = url => {
 /**
  * d-day 계산 (endAt - today)
  */
-const diffDay = due => {
+const diffDay = (due) => {
   const diff = new Date(due) - new Date();
   const remainDay = Math.floor(diff / (1000 * 60 * 60 * 24));
   return `${remainDay}`;
 };
 
-props.recruits.map(d => {
+props.recruits.map((d) => {
   d.dDay = diffDay(d.endAt);
 });
 
@@ -73,43 +84,39 @@ setInterval(diffDay, 1000 * 60 * 60 * 24);
 /**
  * 검색어 색상 변경
  */
-const recruitTitle = ref(null);
+let list = ref([]);
+onMounted(() => {
+  const elements = document.querySelectorAll('.recruit-title');
+  list.value = [...elements];
+});
 
+let words = ref(props.searchWord);
 watch(
   () => props.searchWord,
   (newVal, oldVal) => {
-    let uniCode = ref('');
-    for (const char of newVal) {
-      uniCode = char.charCodeAt();
-    }
+    console.log('newVal', newVal, 'oldVal', oldVal);
+    words.value = newVal;
+    console.log('list.value', list.value);
 
-    // console.log('newVal', newVal);
-    // console.log('uniCode:', uniCode);
+    const sortedList = list.value.filter((item) =>
+      item.innerHTML.includes(props.searchWord)
+    );
+    console.log('sortedList', sortedList);
+    // searchHighlighter(newVal);
 
-    if (newVal !== '') {
-      changeColor(newVal, uniCode);
-    }
-  },
-);
-
-const changeColor = (coloredWord, uniCode) => {
-  console.log('coloredWord', coloredWord, 'uniCode', uniCode);
-
-  recruitTitle.value.map(el => {
-    // for (const char of el.innerHTML) {
-    //   const code = char.charCodeAt();
-    //   console.log('code', code);
-    // }
-
-    if (el.innerHTML.includes(coloredWord)) {
-      console.log('있음');
-      if (coloredWord !== '') {
-        let word = new RegExp(coloredWord, 'g'); // search for all instances
-        let coloredText = el.innerHTML.replace(word, `<span class="text-primary-aqua">${coloredWord}</span>`);
+    sortedList.map((el) => {
+      // console.log('el.innerHTML', el.innerHTML);
+      if (props.searchWord !== '') {
+        let word = new RegExp(props.searchWord, 'g'); // search for all instances
+        let coloredText = el.innerHTML.replace(
+          word,
+          `<span class="text-primary-aqua">${props.searchWord}</span>`
+        );
         el.innerHTML = coloredText;
       }
       console.log('el', el);
-    }
-  });
-};
+      return el;
+    });
+  }
+);
 </script>
