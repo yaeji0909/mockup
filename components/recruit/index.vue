106b184ae43f76897c595f751ff9ec21a179fc6a
rectanglehihat <80513184+rectanglehihat@users.noreply.github.com>
2023-02-03 01:21:42 +0900

    [정효윤] 검색 텍스트 색상 적용 중

--- components/recruit/index.vue
+++ components/recruit/index.vue
@@ -24,13 +24,26 @@
         </div>
         <CommonFooterSelectBox
           :title="$t('recruit.filterSort.newest')"
-          :options="[$t('recruit.filterSort.newest'), $t('recruit.filterSort.byDeadline')]"
+          :options="[
+            $t('recruit.filterSort.newest'),
+            $t('recruit.filterSort.byDeadline'),
+          ]"
           @changeFilter="changeFilter"
         />
       </div>
       <div class="w-[335px] md:w-[710px] xl:w-[1200px] m-auto">
-        <RecruitList :total="total" :recruits="recruits" :searchWord="searchWord" />
-        <CommonPagination class="mt-20" :currentPage="currentPage" @change="changePage" />
+        <RecruitList
+          :total="total"
+          :recruits="recruits"
+          :searchWord="searchWord"
+          :searchTitle="searchTitle"
+        />
+        <CommonPagination
+          class="mt-20"
+          :totalPage="1"
+          :currentPage="currentPage"
+          @change="changePage"
+        />
       </div>
     </section>
     <!-- 복지 -->
@@ -39,7 +52,9 @@
         <h5 class="text-2xl font-medium text-primary-aqua">
           {{ $t('recruit.benefit.companyCulture') }}
         </h5>
-        <h3 class="text-3xl md:text-4xl xl:text-5xl font-bold whitespace-pre-line pt-[10px]">
+        <h3
+          class="text-3xl md:text-4xl xl:text-5xl font-bold whitespace-pre-line pt-[10px]"
+        >
           {{ $t('recruit.benefit.companyCultureDesc') }}
         </h3>
       </div>
@@ -73,7 +88,7 @@ const largerThanSm = breakpoints.greater('sm'); // only larger than sm
 
 let recruits = ref(recruitList);
 let currentPage = ref(1);
-const changePage = param => {
+const changePage = (param) => {
   if (param === 1) recruits.value = recruitList;
   currentPage.value = param;
 };
@@ -83,17 +98,31 @@ const { t } = useI18n();
 /**
  * 직군 select setting
  */
-const departList = recruits.value.map(r => r.department);
+const departList = recruits.value.map((r) => r.department);
 
 const allDepart = departList.length;
-const support = departList.filter(d => d === t('recruit.department.support')).length;
-const planning = departList.filter(d => d === t('recruit.department.planning')).length;
-const development = departList.filter(d => d === t('recruit.department.development')).length;
-const design = departList.filter(d => d === t('recruit.department.design')).length;
-const marketing = departList.filter(d => d === t('recruit.department.marketing')).length;
-const sales = departList.filter(d => d === t('recruit.department.sales')).length;
-const helmet = departList.filter(d => d === t('recruit.department.helmet')).length;
-const cs = departList.filter(d => d === t('recruit.department.cs')).length;
+const support = departList.filter(
+  (d) => d === t('recruit.department.support')
+).length;
+const planning = departList.filter(
+  (d) => d === t('recruit.department.planning')
+).length;
+const development = departList.filter(
+  (d) => d === t('recruit.department.development')
+).length;
+const design = departList.filter(
+  (d) => d === t('recruit.department.design')
+).length;
+const marketing = departList.filter(
+  (d) => d === t('recruit.department.marketing')
+).length;
+const sales = departList.filter(
+  (d) => d === t('recruit.department.sales')
+).length;
+const helmet = departList.filter(
+  (d) => d === t('recruit.department.helmet')
+).length;
+const cs = departList.filter((d) => d === t('recruit.department.cs')).length;
 
 const departOptions = [];
 departOptions.push(
@@ -105,25 +134,31 @@ departOptions.push(
   t('recruit.department.marketing') + ' ' + `(${marketing + '건'})`,
   t('recruit.department.sales') + ' ' + `(${sales + '건'})`,
   t('recruit.department.helmet') + ' ' + `(${helmet + '건'})`,
-  t('recruit.department.cs') + ' ' + `(${cs + '건'})`,
+  t('recruit.department.cs') + ' ' + `(${cs + '건'})`
 );
 
 /**
  * 경력 select setting
  */
-const careerList = recruits.value.map(r => r.career);
+const careerList = recruits.value.map((r) => r.career);
 
 const allCareer = careerList.length;
-const nonRelevant = careerList.filter(d => d === t('recruit.career.nonRelevant')).length;
-const newcomer = careerList.filter(d => d === t('recruit.career.newcomer')).length;
-const experienced = careerList.filter(d => d === t('recruit.career.experienced')).length;
+const nonRelevant = careerList.filter(
+  (d) => d === t('recruit.career.nonRelevant')
+).length;
+const newcomer = careerList.filter(
+  (d) => d === t('recruit.career.newcomer')
+).length;
+const experienced = careerList.filter(
+  (d) => d === t('recruit.career.experienced')
+).length;
 
 const careerOptions = [];
 careerOptions.push(
   t('recruit.career.all') + ' ' + `(${allCareer + '건'})`,
   t('recruit.career.nonRelevant') + ' ' + `(${nonRelevant + '건'})`,
   t('recruit.career.newcomer') + ' ' + `(${newcomer + '건'})`,
-  t('recruit.career.experienced') + ' ' + `(${experienced + '건'})`,
+  t('recruit.career.experienced') + ' ' + `(${experienced + '건'})`
 );
 
 /**
@@ -147,7 +182,7 @@ const filterOption = (division, paramFirst) => {
   if (paramFirst === '전체') {
     recruits.value = recruitList;
   } else {
-    const res = [...recruitList].filter(r => r[division] === paramFirst);
+    const res = [...recruitList].filter((r) => r[division] === paramFirst);
     recruits.value = res;
   }
 };
@@ -155,8 +190,10 @@ const filterOption = (division, paramFirst) => {
 /**
  * latest sort method
  */
-const changeFilter = date => {
-  const res = [...recruitList].sort((a, b) => new Date(a[date]) - new Date(b[date]));
+const changeFilter = (date) => {
+  const res = [...recruitList].sort(
+    (a, b) => new Date(a[date]) - new Date(b[date])
+  );
   recruits.value = res;
 };
 
@@ -164,9 +201,17 @@ const changeFilter = date => {
  * 검색
  */
 let searchWord = ref('');
-const searchInput = e => {
-  const res = [...recruitList].filter(r => r.title.includes(e.target.value));
+let searchTitle = ref([]);
+const searchInput = (e) => {
+  const res = [...recruitList].filter((r) => r.title.includes(e.target.value));
+  console.log('res', res);
   recruits.value = res;
+  searchTitle.value = [];
+  res.map((item) => {
+    console.log('item', item);
+    searchTitle.value.push(item.title);
+  });
+  console.log('searchTitle.value', searchTitle.value);
   // console.log('e.target.value', e.target.value);
   searchWord.value = e.target.value;
 };
--- components/recruit/list/List.vue
+++ components/recruit/list/List.vue
@@ -15,12 +15,24 @@
             {{ recruit.department }}
           </h3>
           <div class="text-left md:w-[40rem] lg:w-[30rem] xl:w-[60rem]">
-            <h1
+            <!-- <h1
               ref="recruitTitle"
               class="recruit-title text-2xl xl:text-3xl font-bold pb-[10px] truncate"
             >
               {{ recruit.title }}
-            </h1>
+            </h1> -->
+            <!--  -->
+            <template v-for="result in highlights">
+              <template v-if="result.match">
+                <span class="text-primary-aqua font-bold">
+                  {{ result.text }}
+                </span>
+              </template>
+              <template v-else>
+                <span class="font-bold">{{ result.text }}</span>
+              </template>
+            </template>
+            <!--  -->
             <div class="flex items-center gap-[15px]">
               <h5
                 class="text-base md:text-xl xl:text-2xl font-medium text-primary-aqua"
@@ -49,6 +61,7 @@ import { breakpointsTailwind, useBreakpoints } from '@vueuse/core';
 const props = defineProps({
   recruits: Array,
   searchWord: String,
+  searchTitle: Array,
 });
 
 /**
@@ -98,25 +111,95 @@ watch(
     words.value = newVal;
     console.log('list.value', list.value);
 
-    const sortedList = list.value.filter((item) =>
-      item.innerHTML.includes(props.searchWord)
-    );
-    console.log('sortedList', sortedList);
-    // searchHighlighter(newVal);
+    // const sortedList = list.value.filter((item) =>
+    //   item.innerHTML.includes(props.searchWord)
+    // );
+    // console.log('sortedList', sortedList);
+    // // searchHighlighter(newVal);
 
-    sortedList.map((el) => {
-      // console.log('el.innerHTML', el.innerHTML);
-      if (props.searchWord !== '') {
-        let word = new RegExp(props.searchWord, 'g'); // search for all instances
-        let coloredText = el.innerHTML.replace(
-          word,
-          `<span class="text-primary-aqua">${props.searchWord}</span>`
-        );
-        el.innerHTML = coloredText;
-      }
-      console.log('el', el);
-      return el;
-    });
+    // sortedList.map((el) => {
+    //   // console.log('el.innerHTML', el.innerHTML);
+    //   if (props.searchWord !== '') {
+    //     let word = new RegExp(props.searchWord, 'g'); // search for all instances
+    //     let coloredText = el.innerHTML.replace(
+    //       word,
+    //       `<span class="text-primary-aqua">${props.searchWord}</span>`
+    //     );
+    //     el.innerHTML = coloredText;
+    //   }
+    //   console.log('el', el);
+    //   return el;
+    // });
+    changeColor(newVal);
   }
 );
+
+let recruitTitle = ref(null);
+const changeColor = (str) => {
+  console.log(
+    "document.querySelectorAll('.recruit-title')",
+    document.querySelectorAll('.recruit-title')
+  );
+
+  const sortedList = list.value.filter((item) =>
+    item.innerHTML.includes(props.searchWord)
+  );
+  // searchHighlighter(newVal);
+
+  sortedList.map((el) => {
+    // console.log('el.innerHTML', el.innerHTML);
+    // if (props.searchWord !== '') {
+    let word = new RegExp(props.searchWord, 'g'); // search for all instances
+    let coloredText = el.innerHTML.replace(
+      word,
+      `<span class="text-primary-aqua">${props.searchWord}</span>`
+      // `<span style="color:#37D1C7">${props.searchWord}</span>`
+    );
+    el.innerHTML = coloredText;
+    // }
+    console.log('el', el);
+  });
+};
+// console.log('list.value', list.value);
+
+const highlights = computed(() => {
+  const results = [];
+  if (props.searchWord && props.searchWord.length > 0) {
+    const regexp = new RegExp(props.searchWord, 'ig');
+
+    // console.log('props.searchTitle', props.searchTitle);
+
+    let start = 0;
+    props.searchTitle.map((item) => {
+      console.log('item', item);
+      for (let match of item.matchAll(regexp)) {
+        results.push({
+          text: item.substring(start, match.index),
+          match: false,
+        });
+        start = match.index;
+        results.push({
+          text: item.substr(start, props.searchWord.length),
+          match: true,
+        });
+        start += props.searchWord.length;
+      }
+
+      if (start < item.length)
+        results.push({
+          text: item.substring(start),
+          match: false,
+        });
+    });
+
+    if (results.length === 0) {
+      results.push({
+        text: item.value,
+        match: false,
+      });
+    }
+  }
+  console.log('results', results);
+  return results;
+});
 </script>
