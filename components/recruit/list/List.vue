e877cd970425061752f7d761a5583ae308a5c13d
정효윤 <rectanglehihat@gmail.com>
2023-02-03 14:42:01 +0900

    [정효윤] 검색 적용

--- components/recruit/list/List.vue
+++ components/recruit/list/List.vue
@@ -9,39 +9,18 @@
         @click="moveToRecruit(recruit.recruitUrl)"
       >
         <div class="flex-row items-center lg:flex pb-[20px] md:pb-0">
-          <h3
-            class="text-base md:text-xl xl:text-2xl font-medium w-40 text-left pb-[20px] md:pb-0"
-          >
+          <h3 class="text-base md:text-xl xl:text-2xl font-medium w-40 text-left pb-[20px] md:pb-0">
             {{ recruit.department }}
           </h3>
           <div class="text-left md:w-[40rem] lg:w-[30rem] xl:w-[60rem]">
-            <!-- <h1
+            <h1
+              v-html="recruit.colorTitle || recruit.title"
               ref="recruitTitle"
               class="recruit-title text-2xl xl:text-3xl font-bold pb-[10px] truncate"
-            >
-              {{ recruit.title }}
-            </h1> -->
-            <!--  -->
-            <template v-for="result in highlights">
-              <template v-if="result.match">
-                <span class="text-primary-aqua font-bold">
-                  {{ result.text }}
-                </span>
-              </template>
-              <template v-else>
-                <span class="font-bold">{{ result.text }}</span>
-              </template>
-            </template>
-            <!--  -->
+            />
             <div class="flex items-center gap-[15px]">
-              <h5
-                class="text-base md:text-xl xl:text-2xl font-medium text-primary-aqua"
-              >
-                D-{{ recruit.dDay }}
-              </h5>
-              <h5 class="text-xs md:text-sm xl:text-base text-gray-sub">
-                {{ recruit.startAt }} ~ {{ recruit.endAt }}
-              </h5>
+              <h5 class="text-base md:text-xl xl:text-2xl font-medium text-primary-aqua">D-{{ recruit.dDay }}</h5>
+              <h5 class="text-xs md:text-sm xl:text-base text-gray-sub">{{ recruit.startAt }} ~ {{ recruit.endAt }}</h5>
             </div>
           </div>
         </div>
@@ -60,8 +39,6 @@ import { breakpointsTailwind, useBreakpoints } from '@vueuse/core';
 
 const props = defineProps({
   recruits: Array,
-  searchWord: String,
-  searchTitle: Array,
 });
 
 /**
@@ -73,7 +50,7 @@ const largerThanSm = breakpoints.greater('sm'); // only larger than sm
 /**
  * 해당 공고로 이동
  */
-const moveToRecruit = (url) => {
+const moveToRecruit = url => {
   console.log('리크루트로 이동');
   if (!url) return;
 };
@@ -81,13 +58,13 @@ const moveToRecruit = (url) => {
 /**
  * d-day 계산 (endAt - today)
  */
-const diffDay = (due) => {
+const diffDay = due => {
   const diff = new Date(due) - new Date();
   const remainDay = Math.floor(diff / (1000 * 60 * 60 * 24));
   return `${remainDay}`;
 };
 
-props.recruits.map((d) => {
+props.recruits.map(d => {
   d.dDay = diffDay(d.endAt);
 });
 
@@ -101,105 +78,5 @@ let list = ref([]);
 onMounted(() => {
   const elements = document.querySelectorAll('.recruit-title');
   list.value = [...elements];
-});
-
-let words = ref(props.searchWord);
-watch(
-  () => props.searchWord,
-  (newVal, oldVal) => {
-    console.log('newVal', newVal, 'oldVal', oldVal);
-    words.value = newVal;
-    console.log('list.value', list.value);
-
-    // const sortedList = list.value.filter((item) =>
-    //   item.innerHTML.includes(props.searchWord)
-    // );
-    // console.log('sortedList', sortedList);
-    // // searchHighlighter(newVal);
-
-    // sortedList.map((el) => {
-    //   // console.log('el.innerHTML', el.innerHTML);
-    //   if (props.searchWord !== '') {
-    //     let word = new RegExp(props.searchWord, 'g'); // search for all instances
-    //     let coloredText = el.innerHTML.replace(
-    //       word,
-    //       `<span class="text-primary-aqua">${props.searchWord}</span>`
-    //     );
-    //     el.innerHTML = coloredText;
-    //   }
-    //   console.log('el', el);
-    //   return el;
-    // });
-    changeColor(newVal);
-  }
-);
-
-let recruitTitle = ref(null);
-const changeColor = (str) => {
-  console.log(
-    "document.querySelectorAll('.recruit-title')",
-    document.querySelectorAll('.recruit-title')
-  );
-
-  const sortedList = list.value.filter((item) =>
-    item.innerHTML.includes(props.searchWord)
-  );
-  // searchHighlighter(newVal);
-
-  sortedList.map((el) => {
-    // console.log('el.innerHTML', el.innerHTML);
-    // if (props.searchWord !== '') {
-    let word = new RegExp(props.searchWord, 'g'); // search for all instances
-    let coloredText = el.innerHTML.replace(
-      word,
-      `<span class="text-primary-aqua">${props.searchWord}</span>`
-      // `<span style="color:#37D1C7">${props.searchWord}</span>`
-    );
-    el.innerHTML = coloredText;
-    // }
-    console.log('el', el);
-  });
-};
-// console.log('list.value', list.value);
-
-const highlights = computed(() => {
-  const results = [];
-  if (props.searchWord && props.searchWord.length > 0) {
-    const regexp = new RegExp(props.searchWord, 'ig');
-
-    // console.log('props.searchTitle', props.searchTitle);
-
-    let start = 0;
-    props.searchTitle.map((item) => {
-      console.log('item', item);
-      for (let match of item.matchAll(regexp)) {
-        results.push({
-          text: item.substring(start, match.index),
-          match: false,
-        });
-        start = match.index;
-        results.push({
-          text: item.substr(start, props.searchWord.length),
-          match: true,
-        });
-        start += props.searchWord.length;
-      }
-
-      if (start < item.length)
-        results.push({
-          text: item.substring(start),
-          match: false,
-        });
-    });
-
-    if (results.length === 0) {
-      results.push({
-        text: item.value,
-        match: false,
-      });
-    }
-  }
-  console.log('results', results);
-  return results;
 });
 </script>
