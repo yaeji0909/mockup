83b4fd0f3522dcfce007ff5e7be14c540dcb2f9b
정효윤 <rectanglehihat@gmail.com>
2023-02-08 13:08:59 +0900

    [정효윤] css 수정

--- pages/recruit/index.vue
+++ pages/recruit/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-center">
-    <CommonNavigationBar class="fixed z-10 w-screen" :elementTop="elementTop" color="white" />
+    <CommonNavigationBar class="fixed z-10 w-screen" color="white" />
     <main>
-      <section>
+      <section class="pt-28 md:pt-32">
         <h1 class="text-4xl md:text-6xl xl:text-7xl font-bold whitespace-pre-line">Recruit</h1>
         <h5 class="text-base xl:text-xl mt-[10px]">{{ $t('recruit.desc') }}</h5>
       </section>
@@ -14,6 +14,4 @@
   </div>
 </template>
 
-<script setup>
-const elementTop = ref([]);
-</script>
+<script setup></script>
