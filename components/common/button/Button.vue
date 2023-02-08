759e24019041d55d9a6b8c96e9b187f59121e27e
정효윤 <rectanglehihat@gmail.com>
2023-02-08 13:22:26 +0900

    [정효윤] css 수정

--- components/common/button/Button.vue
+++ components/common/button/Button.vue
@@ -3,7 +3,7 @@
     :class="[
       'text-xs md:text-base font-bold rounded-full',
       icon
-        ? 'flex justify-center items-center gap-2.5 w-[180px] h-[45px] md:h-[50px] bg-white/30 hover:bg-white hover:text-black '
+        ? 'flex justify-center items-center gap-2.5 w-[160px] md:w-[180px] h-[45px] md:h-[50px] bg-white/30 hover:bg-white hover:text-black '
         : 'w-[190px] h-[45px] md:h-[58px] hover:bg-gradient-to-r from-primary-aqua to-primary-mint hover:border-none hover:text-white ',
       ,
       `bg-${bgColor}` || 'bg-white/30',
