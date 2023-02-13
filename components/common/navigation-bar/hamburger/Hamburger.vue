25181776be0690da3c47676987c7044190df4bc3
정효윤 <rectanglehihat@gmail.com>
2023-02-13 13:41:11 +0900

    [정효윤] duration 변경

--- components/common/navigation-bar/hamburger/Hamburger.vue
+++ components/common/navigation-bar/hamburger/Hamburger.vue
@@ -1,23 +1,8 @@
 <template>
   <div class="md:hidden flex-col mx-2" @click="clickHandler">
-    <div
-      id="line1"
-      ref="line1"
-      class="p-[0.1rem] w-5 rounded bg-white"
-      :class="`bg-${color}`"
-    ></div>
-    <div
-      id="line2"
-      ref="line2"
-      class="mt-[0.1rem] p-[0.1rem] w-5 rounded bg-white"
-      :class="`bg-${color}`"
-    ></div>
-    <div
-      id="line3"
-      ref="line3"
-      class="mt-[0.1rem] p-[0.1rem] w-5 rounded bg-white"
-      :class="`bg-${color} `"
-    ></div>
+    <div id="line1" ref="line1" class="p-[0.1rem] w-5 rounded bg-white" :class="`bg-${color}`"></div>
+    <div id="line2" ref="line2" class="mt-[0.1rem] p-[0.1rem] w-5 rounded bg-white" :class="`bg-${color}`"></div>
+    <div id="line3" ref="line3" class="mt-[0.1rem] p-[0.1rem] w-5 rounded bg-white" :class="`bg-${color} `"></div>
   </div>
 </template>
 
@@ -36,18 +21,18 @@ const props = defineProps({
 
 watch(
   () => props.showMenu,
-  (newShowMenu) => {
+  newShowMenu => {
     clickHandler(newShowMenu);
-  }
+  },
 );
 
-const clickHandler = (show) => {
+const clickHandler = show => {
   if (!show) {
     gsap.to('#line1', { rotation: 0 });
     gsap.to('#line3', { rotation: 0 });
-    gsap.to('#line2', { display: 'block' });
+    gsap.to('#line2', { display: 'block', duration: 0.01 });
   } else {
-    gsap.to('#line2', { display: 'none', duration: 0.1 });
+    gsap.to('#line2', { display: 'none', duration: 0.01 });
     gsap.to('#line1', {
       rotation: 45,
       // transformOrigin: 'center center',
